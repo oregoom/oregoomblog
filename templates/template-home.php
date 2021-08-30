e88827089edc1c707c1322a2171e06f0a037cb0c
Nivardo <nivardo.choqque@oregoom.com>
2021-08-30 17:26:43 -0500

    Corregido los errores

--- templates/template-home.php
+++ templates/template-home.php
@@ -11,19 +11,64 @@ if(have_posts()){
     while(have_posts()) : the_post(); ?>
 
 
-<main class="container mt-5">
-    
-    <div class="row">
-        
-        <aside class="col col-xxl-3 col-xl-4 col-lg-3 d-none d-lg-block">
-        
-            <div class="list-group list-group-flush">
-                
-                <span class="h5"><strong>Contenido</strong></span>
-                
+<main class="container pt-4">
+
+
+    <h1 class="text-center"><?php the_title(); ?></h1>
+
+
+
+    <!--//GOOGLE ADSENSE (PC) -->
+    <?php if(get_option('template_oregoom_adsense_728_90') != ''){ ?>                
+        <div class="pb-3 pt-3 text-center d-none d-lg-block">
+
+            <?php  echo get_option('template_oregoom_adsense_728_90'); ?>
+
+        </div>                
+    <?php } ?>
+
+    <!--//GOOGLE ADSENSE (Movil) -->
+    <?php if(get_option('template_oregoom_adsense_auto') != ''){ ?>                
+        <div class="pb-3 pt-3 d-lg-none text-center">
+
+            <?php  echo get_option('template_oregoom_adsense_auto'); ?>
+
+        </div>                
+    <?php } ?>
+
+
+
+    <div class="pt-3 pb-4">
+        <?php the_content(); ?>   
+    </div>
+
+
+    <!--//GOOGLE ADSENSE (PC) -->
+    <?php if(get_option('template_oregoom_adsense_728_90') != ''){ ?>                
+        <div class="pb-3 pt-3 text-center d-none d-lg-block">
+
+            <?php  echo get_option('template_oregoom_adsense_728_90'); ?>
+
+        </div>                
+    <?php } ?>
+
+    <!--//GOOGLE ADSENSE (Movil) -->
+    <?php if(get_option('template_oregoom_adsense_auto') != ''){ ?>                
+        <div class="pb-3 pt-3 d-lg-none text-center">
+
+            <?php  echo get_option('template_oregoom_adsense_auto'); ?>
+
+        </div>                
+    <?php } ?>
+
+
+
+
+    <div class="pt-5">
+
+        <div class="row pt-3 pb-3">
+
                 <?php 
-                
-                $ID_post = get_the_ID();
 
                 //Consulta que pertenece a una categoria específica 
                 $wordpress_query = new WP_Query( array(
@@ -34,118 +79,58 @@ if(have_posts()){
                     ));   
 
                 while ($wordpress_query->have_posts()) : $wordpress_query->the_post(); 
-                
-                    // Título corto
-                    if(get_post_meta(get_the_ID(), 'oregoom_title_corto_post', true)){ 
 
-                        $oregoom_title_corto_post = get_post_meta(get_the_ID(), 'oregoom_title_corto_post', true); ?>              
+                    if(has_post_thumbnail()){ ?>
 
-                        <a class="list-group-item list-group-item-action <?php if($ID_post == get_the_ID()){ echo "list-group-item-secondary"; } ?>" target="_self" href="<?php the_permalink(); ?>"><?php echo $oregoom_title_corto_post; ?></a> <?php
-                        
-                    } else { ?>
-                         
-                        <a class="list-group-item list-group-item-action <?php if($ID_post == get_the_ID()){ echo "list-group-item-secondary"; } ?>" target="_self" href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <?php
+                    <div class="col-xl-4 col-lg-6 col-md-4 col-sm-6 pb-3">
 
-                    }
-                
+                        <div class="shadow-lg rounded">  
+                            
+                            <a class="text-dark" href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail('full', array( 'class' => 'img-fluid' )); ?>
+                            </a>
+
+                            <div class="p-4">
+                                <a class="text-dark" href="<?php the_permalink(); ?>">
+                                    <h3 class="h5" style="font-family: Raleway, sans-serif; line-height: 1.2em;">
+                                        <strong><?php the_title(); ?></strong>
+                                    </h3>
+                                </a>
+                                <p> <?php the_excerpt(); ?> </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php }                    
+
                 endwhile;
-                
+
                 wp_reset_postdata(); ?>
-                    
-            </div>
-            
-        </aside>
-        
-        <article class="col">
-            
-            <div class="container" style="max-width: 760px; margin-left: auto; margin-right: auto;">
-                
-                <h1 class="text-center h1" style="font-weight: bold;"><?php the_title(); ?></h1>
-                
-                <div class="pt-4 pb-4">
-                    <?php the_content(); ?>   
-                </div> 
-                
-            </div>
-            
-        </article>
-        
-        <div class="col col-2 d-none d-xxl-block">
-            
-            <!--//GOOGLE ADSENSE (PC) -->
-            <?php if(get_option('template_oregoom_adsense_160_600') != ''){ ?>                
-                <div class="pb-3 text-end">
 
-                    <?php  echo get_option('template_oregoom_adsense_160_600'); ?>
-
-                </div>                
-            <?php } ?>
-            
         </div>
-        
-    </div>    
-    
+
+    </div>
+
+
+
+    <div class="container pt-5 pb-5">
+
+        <!--//GOOGLE ADSENSE (PC y Movil) -->
+        <?php if(get_option('template_oregoom_adsense_auto') != ''){ ?>                
+            <div class="pb-3 pt-3 text-center">
+
+                <?php  echo get_option('template_oregoom_adsense_auto'); ?>
+
+            </div>                
+        <?php } ?>
+
+    </div>
+
+
+
 </main>
 
-<div class="container mt-4 mb-5">
-    
-    <div class="row">
-        
-        <div class="col">
-            
-            <!--//GOOGLE ADSENSE (PC) -->
-            <?php if(get_option('template_oregoom_adsense_300_250') != ''){ ?>                
-                <div class="pb-3 text-center">
 
-                    <?php  echo get_option('template_oregoom_adsense_300_250'); ?>
-
-                </div>                
-            <?php } ?>
-            
-        </div>
-        
-        <div class="col d-none d-lg-block">
-            
-            <!--//GOOGLE ADSENSE (PC) -->
-            <?php if(get_option('template_oregoom_adsense_300_250') != ''){ ?>                
-                <div class="pb-3 text-center">
-
-                    <?php  echo get_option('template_oregoom_adsense_300_250'); ?>
-
-                </div>                
-            <?php } ?>
-            
-        </div>
-        
-        <div class="col d-none d-xl-block">
-            
-            <!--//GOOGLE ADSENSE (PC) -->
-            <?php if(get_option('template_oregoom_adsense_300_250') != ''){ ?>                
-                <div class="pb-3 text-center">
-
-                    <?php  echo get_option('template_oregoom_adsense_300_250'); ?>
-
-                </div>                
-            <?php } ?>
-            
-        </div>
-        
-        <div class="col d-none d-xxl-block">
-            
-            <!--//GOOGLE ADSENSE (PC) -->
-            <?php if(get_option('template_oregoom_adsense_300_250') != ''){ ?>                
-                <div class="pb-3 text-center">
-
-                    <?php  echo get_option('template_oregoom_adsense_300_250'); ?>
-
-                </div>                
-            <?php } ?>
-            
-        </div>
-        
-    </div>
-    
-</div>
 
 <?php
     
